451e8e1a98e52a66783a5708361b0a0eed600b25
Fabio Nascimento <fabiogtr@hotmail.com>
2022-11-18 19:49:21 -0300

    Projeto criado

---
+++ src/test/java/br/com/fabiosnascimento/junitapi/JunitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabiosnascimento.junitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
